150adbee5aa2c9b07a6403d331bd4bbce0865b60
Zach Ghera <zghera@google.com>
2021-06-15 22:19:07 +0000

    Common utils for offline test and benchmarks.
    The utilities created are used by both tests/UnwindOfflinetest.cpp and
    OfflineUnwindBenchmarks.cpp to help perform offline unwinds. This
    utility module was largely taken from the offline test fixture in
    tests/UnwindOfflineTests.cpp.
    
    Test: Refactoring CL. Benchmarks still run and unit tests still pass.
    Change-Id: Ibb846930bcc2b78f3ff1f16dd007d259cfb38e22

--- libunwindstack/utils/OfflineUnwindUtils.h
+++ libunwindstack/utils/OfflineUnwindUtils.h
@@ -19,9 +19,57 @@
 
 #include <string>
 
+#include <unwindstack/MachineArm.h>
+#include <unwindstack/MachineArm64.h>
+#include <unwindstack/MachineX86.h>
+#include <unwindstack/MachineX86_64.h>
+#include <unwindstack/Maps.h>
+#include <unwindstack/RegsArm.h>
+#include <unwindstack/RegsArm64.h>
+#include <unwindstack/RegsX86.h>
+#include <unwindstack/RegsX86_64.h>
+#include <unwindstack/Unwinder.h>
+
+#include "MemoryOffline.h"
+
 namespace unwindstack {
 
 std::string GetOfflineFilesDirectory();
+
+std::string DumpFrames(const Unwinder& unwinder);
+
+bool AddMemory(std::string file_name, MemoryOfflineParts* parts, std::string& error_msg);
+
+class OfflineUnwindUtils {
+ protected:
+  bool Init(const std::string& offline_files_dir, ArchEnum arch, std::string& error_msg,
+            bool add_stack = true, bool set_maps = true);
+
+  bool ResetMaps(std::string& error_msg);
+
+  bool SetProcessMemory(std::string& error_msg);
+
+  bool SetJitProcessMemory(std::string& error_msg);
+
+  bool SetRegs(ArchEnum arch, std::string& error_msg);
+
+  template <typename AddressType>
+  bool ReadRegs(RegsImpl<AddressType>* regs,
+                const std::unordered_map<std::string, uint32_t>& name_to_reg,
+                std::string& error_msg);
+
+  static std::unordered_map<std::string, uint32_t> arm_regs_;
+  static std::unordered_map<std::string, uint32_t> arm64_regs_;
+  static std::unordered_map<std::string, uint32_t> x86_regs_;
+  static std::unordered_map<std::string, uint32_t> x86_64_regs_;
+
+  std::string cwd_;
+  std::string offline_dir_;
+  std::string map_buffer;
+  std::unique_ptr<Regs> regs_;
+  std::unique_ptr<Maps> maps_;
+  std::shared_ptr<Memory> process_memory_;
+};
 
 }  // namespace unwindstack
 
